0a9d3cda28315bc2c513d10469edee5de1e818ab
Dixon Chacón <gocho.14@gmail.com>
2024-12-01 02:51:21 -0400

    Multiple changes:
    - Audited packges.
    - Added types, and d3 packages.
    - Enabled React.StrictMode.

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import { App } from './app';
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render((<App />), document.getElementById('reactMountPoint'));
+  ReactDOM.render(
+    (<React.StrictMode>
+        <App />
+    </React.StrictMode>), document.getElementById('reactMountPoint'));
 });
